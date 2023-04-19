d9b6c2e6baf3a5add1a50365240c87366550ac30
Steven Hoang <56275718+xcaracal@users.noreply.github.com>
2023-04-18 17:03:22 -0700

    updates

--- triangle.js
+++ triangle.js
@@ -8,36 +8,62 @@ function triangleOutput(){
     const a = parseFloat(document.getElementById('value1').value);
     const b = parseFloat(document.getElementById('value2').value);
     const c = parseFloat(document.getElementById('value3').value);
-    if (checkTriangle(a,b,c) === True ){
-        /*TODO */
+    if (checkTriangle(a,b,c) === true ){
+        /* output the triangle calculations to the <p id = "output"></p>. change the output to appropriate sentence  .use the functions below like the get triangle type and perimeter and acute right obsolete also use get triangle angles*/
+        /* make sure the output stays on screen*/
+        document.getElementById('output').innerHTML = "The triangle is " + getTriangleType(a,b,c) + " and its perimeter is " + perimeter(a,b,c) + " and its acute right is " + acuteRightObtuse(a,b,c);
     }
     else{
-        return "The given sides do not form a triangle";
+        document.getElementById('output').innerHTML = "The triangle is not a triangle";
     }
 }
-
 
 /* TODO - Below are suggested functions .
 You do not have to use them, but it is recommended */
 
 /*  Check if triangle */
 function checkTriangle(side1, side2, side3){
-
+    if (side1 === side2 && side1 === side3){
+        return true;
+    }
+    else if (side1 === side2 || side1 === side3 || side2 === side3){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 /* Check if Equilateral, Isosceles or Scalene */
 function getTriangleType(side1,side2,side3){
-
+    if (side1 === side2 && side1 === side3){
+        return "Equilateral";
+    }
+    else if (side1 === side2 || side1 === side3 || side2 === side3){
+        return "Isosceles";
+    }
+    else{
+        return "Scalene";
+    }
 }
 
 
 /* Calculate perimeter */
 function perimeter (side1, side2, side3) {
-
+    const perimeter = side1 + side2 + side3;
+    return perimeter;
 }
 /* Check if acute, right or obtuse */
 function acuteRightObtuse (side1, side2, side3) {
-
+    if (side1 === side2 && side1 === side3){
+        return "Acute";
+    }
+    else if (side1 === side2 || side1 === side3 || side2 === side3){
+        return "Right";
+    }
+    else{
+        return "Obtuse";
+    }
 }
 
 /* Function that gets the triangle angles*/
@@ -55,10 +81,14 @@ function getTriangleAngles(side1, side2, side3){
 
 /* Calculate the area */
 function getArea(side1, side2, side3){
-
+    const area = (side1 + side2 + side3) / 2;
+    return area;
 }
 
-/* TODO - Create the Event listener, which calls the result of triangleOutput() 
+/* TODO - Create the Event listener, which calls the result of triangleOutput() . keep output on screen*/
+document.getElementById('submit').addEventListener('click', triangleOutput);
+
+
 
 
 
